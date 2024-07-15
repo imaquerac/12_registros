97263c24830c6840738d79344e058deef32e84cf
imaquerac <imaquerac@unjbg.edu.pe>
2024-07-15 06:51:49 -0500

    se agrega el tercer archivo

--- registros3.cpp
+++ registros3.cpp
@@ -1,5 +1,43 @@
-#include<iostream>
+#include <iostream>
+#include <string>
 using namespace std;
+
+struct persona{
+    string nombre;
+    string diaNacimiento;
+    int mesNacimiento;
+    int anioNacimiento;
+};
+
 int main(){
-	
+    persona personas[100];
+    int n;
+    cout<<"Ingrese el numero de personas: ";cin>>n;
+    cin.ignore();
+    for(int i = 0; i < n; i++){
+        cout<<"Ingrese datos de la persona "<< i + 1 <<endl;
+    	cout<<"Nombres: ";
+        getline(cin, personas[i].nombre);
+        cout<<"Dia de nacimiento: ";cin>>personas[i].diaNacimiento;
+        cout<<"Mes de nacimiento: ";cin>>personas[i].mesNacimiento;
+        cout<<"Anio de nacimiento: ";cin>>personas[i].anioNacimiento;
+        cin.ignore();
+    }
+    system("cls");
+    int op;
+    do
+    {
+        cout<<"Ingrese el numero de mes que desea mostrar (Para salir ingresar 0): ";
+        cin>>op;
+        for(int i = 0; i < n; i++)
+        {
+            if(personas[i].mesNacimiento == op)
+            {
+                cout<<"Nombre : "<<personas[i].nombre << endl;
+                cout<<"Fecha de nacimiento : "<<personas[i].diaNacimiento<< "/" <<personas[i].mesNacimiento<< "/" <<personas[i].anioNacimiento<<endl;
+                cout<<"---------------------------------------------<<endl;
+            }
+        }
+    } while(op != 0);
+    return 0;
 }
